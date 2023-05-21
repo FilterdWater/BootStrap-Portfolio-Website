1525749febe561c73c29ffef0db15557e00a0c6a
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2023-05-21 17:11:02 +0200

    Added fade-in JavaScript
    Added fade in on observation see fade-in.js

---
+++ WebPages_v2/fade-in.js
@@ -0,0 +1,25 @@
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show-1');
+            observer1.unobserve(entry.target);
+        }
+    });
+},{threshold: 0.33});
+
+const hiddenElements1 = document.querySelectorAll('.hidden-1');
+hiddenElements1.forEach((el) => observer1.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show-2');
+            observer2.unobserve(entry.target);
+        }
+    });
+},{threshold: 0.33});
+
+const hiddenElements2 = document.querySelectorAll('.hidden-2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+// https://www.youtube.com/watch?v=T33NN_pPeNI&t=30s is de gebruikt om deze javascript te maken
